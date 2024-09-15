b690d081b0581c2fa7695b6a72fe25a5b3f43c05
romanshilpakar <iamromanshilpakar@gmail.com>
2024-09-15 22:36:19 +0545

    FEAT: Dashboard Template added

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,10 +1,9 @@
 import React from "react";
+import AdminDashboard from "./components/admin-dashboard";
 
 const Admin = () => {
   return (
-    <div>
-      Admin Dashboard
-    </div>
+    <AdminDashboard/>
   );
 };
 
